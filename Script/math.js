c8667543fb2768436afe1efa9eb23361cca07e76
Carlos Perales <carlosperales050@gmail.com>
2023-01-24 14:10:53 -0600

    Se agrega mas formulas

--- Script/math.js
+++ Script/math.js
@@ -47,4 +47,66 @@ function calcularTriangulo(lado1,lado2,base,altura){
     }
 }
 
+function calcularAlturaTriangulo(lado1,lado2,base){
+    if((lado1 == lado2) & (lado1!=base) & (lado2!=base)){
+        
+         //formula
+         return Math.sqrt( (lado1**2)-( Math.pow(lado2,2) /4) )
+
+
+    }else{
+        console.warm('Este es no es un triangulo isosceles');
+    }
+}
+function calcularAlturaEscaleno(lado1,lado2,base){
+    if((lado1 != lado2) & (lado1 != base) & (lado2 != base)){
+        //formula
+        //sacamos el semiperimertro (a+b+c)/2
+        const semiperimertro =(lado1+lado2+base)/2;
+        const division=(2/lado1);
+        const resta1= semiperimertro-lado1;
+        const resta2= semiperimertro-lado2;
+        const resta3= semiperimertro-base;
+    
+       //const altura =division*Math.sqrt((semiperimertro)*(resta1)* (resta2 )*( resta3))
+      
+      return Math.trunc(division*Math.sqrt((semiperimertro)*(resta1)* (resta2 )*( resta3)))
+      // return Math.floor(altura);
+    }else{
+        console.warm('Este es no es un triangulo isosceles');
+    }
+}
+
+
+
 console.groupEnd('triangulo');
+
+console.group('cirle');
+
+const radioCircle=3;
+const diametroCircle=radioCircle*2;
+const PI=3.1416;
+
+const circunferencia=diametroCircle*PI;
+const areaCircle=(radioCircle**2)*PI;// '**' = '^' potencia /
+
+console.log({
+    radioCircle,
+    diametroCircle,
+    PI,
+    circunferencia,
+    areaCircle
+})
+
+function calcularCircle(radio){
+    const diametro= radio*2;
+    const radioAlCuadrador=Math.pow(radio,2);//lo mismo que poner radio elevado al cuadrado
+    return {
+        circunferencia:diametro*Math.PI, //Math.PI 
+        area:radioAlCuadrador*Math.PI.toFixed(3)//para saber cuantos numeros despues del punto
+    }
+}
+
+
+
+console.groupEnd('circle');
